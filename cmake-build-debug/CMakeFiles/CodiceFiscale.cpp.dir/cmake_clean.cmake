8992414c535dc749af0bc045f5348371778226ae
Marian <marianberzuntanu@gmail.comm>
2025-07-12 09:48:13 +0200

    creato cognome

---
+++ cmake-build-debug/CMakeFiles/CodiceFiscale.cpp.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CodiceFiscale.cpp.dir/main.cpp.obj"
+  "CMakeFiles/CodiceFiscale.cpp.dir/main.cpp.obj.d"
+  "CodiceFiscale.cpp.exe"
+  "CodiceFiscale.cpp.exe.manifest"
+  "CodiceFiscale.cpp.pdb"
+  "libCodiceFiscale.cpp.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CodiceFiscale.cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
