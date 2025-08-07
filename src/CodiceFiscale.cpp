82d12716786a0a141305f81d74ec625e40642236
Marian <marianberzuntanu@gmail.com>
2025-08-08 00:00:31 +0200

    little changes

--- src/CodiceFiscale.cpp
+++ src/CodiceFiscale.cpp
@@ -176,7 +176,6 @@ int GiornoNascita(const int &giorno, const bool &sesso) {
     return res;
 }
 
-
 unordered_map<string, string> caricaCodici(const string &nomeFile) {
     unordered_map<string, string> codici;
     ifstream file(nomeFile);
@@ -217,7 +216,6 @@ string ZonaNascita(const string &zona) {
     }
 }
 
-
 string caratteriPari(const string &cf_temp) {
     vector<char> stringa_pari;
     for (int i = 0; i < cf_temp.size(); ++i) {
@@ -239,7 +237,6 @@ string caratteriDispari(const string &cf_temp) {
     string sdispari(stringa_dispari.begin(), stringa_dispari.end());
     return sdispari;
 }
-
 
 //CONVERSIONE PER CODICE DI CONTROLLO
 unordered_map<char, int> caricaMappaControllo(const string &nomeFile) {
@@ -333,4 +330,46 @@ string CarattereControllo(const string &cf_temp) {
     char carattereFinale = trovaCarattere(resto, mappaConversione);
     string ccfinale(1, carattereFinale);
     return ccfinale;
+}
+
+string returnCodiceFiscale() {
+    string cogno;
+    string name;
+    string zona;
+    bool sesso;
+    int mese;
+    int giorno;
+    int ann;
+    cout << "Inserisci Cognome: " << endl;
+    getline(cin, cogno);
+    transform(cogno.begin(), cogno.end(), cogno.begin(), ::toupper);
+    cout << "Inserisci Nome: " << endl;
+    getline(cin, name);
+    transform(name.begin(), name.end(), name.begin(), ::toupper);
+    cout << "Inserisci Anno di Nascita: " << endl;
+    cin >> ann;
+    cout << "Inserisci Mese di Nascita (1-12): " << endl;
+    cin >> mese;
+    cout << "Inserisci Giorno di Nascita (1-31): " << endl;
+    cin >> giorno;
+    cout << "Inserisci Sesso (1=Maschile/0=Femminile): " << endl;
+    cin >> sesso;
+    cin.ignore();
+    cout << "Inserisci Comune di Nascita: " << endl;
+    getline(cin, zona);
+    zona = normalizzaNome(zona);
+
+
+    string x1 = Cognome(cogno);
+    string x2 = Nome(name);
+    string temp = to_string(ann);
+    string x3 = temp.substr(temp.length() - 2);
+    string x4 = MeseNascita(mese);
+    string x5 = to_string(GiornoNascita(giorno, sesso));
+    string x6 = ZonaNascita(zona);
+    string tot = x1 + x2 + x3 + x4 + x5 + x6;
+    string x7 = CarattereControllo(tot);
+
+    string cf_finale = x1 + x2 + x3 + x4 + x5 + x6 + x7;
+    return cf_finale;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -5,45 +5,9 @@ using namespace std;
 
 int main()
 {
-    string cogno;
-    string name;
-    string zona;
-    bool sesso;
-    int mese;
-    int giorno;
-    int ann;
-    cout << "Inserisci Cognome: " << endl;
-    getline(cin, cogno);
-    transform(cogno.begin(),cogno.end(),cogno.begin(),::toupper);
-    cout << "Inserisci Nome: " << endl;
-    getline(cin,name);
-    transform(name.begin(),name.end(),name.begin(),::toupper);
-    cout << "Inserisci Anno di Nascita: " << endl;
-    cin >> ann;
-    cout << "Inserisci Mese di Nascita (1-12): " << endl;
-    cin >> mese;
-    cout << "Inserisci Giorno di Nascita (1-31): " << endl;
-    cin >> giorno;
-    cout << "Inserisci Sesso (1=Maschile/0=Femminile): " << endl;
-    cin >> sesso;
-    cin.ignore();
-    cout << "Inserisci Comune di Nascita: " << endl;
-    getline(cin, zona);
-    zona = normalizzaNome(zona);
+    
+    string cfinale = returnCodiceFiscale();
+    cout << "Codice Fiscale: " << cfinale << endl;
 
-
-    string x1 = Cognome(cogno);
-    string x2 = Nome(name);
-    string temp = to_string(ann);
-    string x3 = temp.substr(temp.length() - 2);
-    string x4 = MeseNascita(mese);
-    string x5 = to_string(GiornoNascita(giorno, sesso));
-    string x6 = ZonaNascita(zona);
-    string tot = x1+x2+x3+x4+x5+x6;
-    string x7 = CarattereControllo(tot);
-
-    string cf_finale = x1+x2+x3+x4+x5+x6+x7;
-    cout << "Codice Fiscale: " << cf_finale << endl;
-
-
+    return 0;
 }
